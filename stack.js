b06af700253a615626e027ac997ec4d2c593737b
Rootjang92 <janggeunho7@gmail.com>
2018-07-27 15:39:59 +0900

    stack file updata

--- stack.js
+++ stack.js
@@ -4,3 +4,16 @@
 // 스택 2개. a에서 pop한 녀석을 b에서 받게 한 후. a에 하나씩 가져와서 pop.
 // 2개의 stack으로 구현.
 
+function Stack() {
+ var dataStack = [];
+
+ this.push = function(element) {
+  dataStack.push(element);
+ }
+ this.peek = function() {
+   return dataStack[0];
+ }
+ this.size = function() {
+   return dataStack.length;
+ }
+} //
