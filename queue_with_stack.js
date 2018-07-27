20112bf4e0ca74afaf9c906a3b70687598f68808
Rootjang92 <janggeunho7@gmail.com>
2018-07-27 15:22:52 +0900

    queue with stack

---
+++ queue_with_stack.js
@@ -0,0 +1,14 @@
+function Queue_stack() {
+  var stack1 = [];
+  var stack2 = [];
+
+  this.enqueue = function(element) {
+    stack1.push(element);
+  };
+
+  this.dequeue = function() {
+    if (stack2.length === 0) {
+
+    }
+  }
+}
