3e57c5ef5fc7d8ec47bb0a2c79ab8434b6e74deb
Rootjang92 <janggeunho7@gmail.com>
2018-07-27 16:19:38 +0900

    2 stack result queue

--- queue_with_stack.js
+++ queue_with_stack.js
@@ -3,12 +3,31 @@ function Queue_stack() {
   var stack2 = [];
 
   this.enqueue = function(element) {
-    stack1.push(element);
+    return stack1.push(element);
   };
 
   this.dequeue = function() {
-    if (stack2.length === 0) {
-
+    if (stack2.length > 0) {
+      return stack2.pop();
     }
-  }
+    var result = stack1.pop();
+    while (stack1.length > 1) {
+      stack1.push(stack2.pop());
+    }
+    return result;
+  };
 }
+
+var queue = new Queue_stack();
+// var enqueue = queue.enqueue();
+var dequeue = queue.dequeue();
+
+console.log(queue.enqueue(1));
+console.log(queue.enqueue(2));
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.enqueue(1));
+
+
+
